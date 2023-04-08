b4ee1eabcb810574409b65d284e09cefa76d1f1b
rafaldzielak <69675572+rafaldzielak@users.noreply.github.com>
2023-04-08 10:24:16 +0200

    Add header with auth

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,6 +4,7 @@ const config = {
   theme: {
     extend: {},
   },
+  // plugins: [require("@tailwindcss/typography"), require("daisyui")],
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
 };
 
